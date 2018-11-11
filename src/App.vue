20b2b40df50d5e33edd3ab728bb0d6b489c6e0e6
fi1tch <vilkovaliza@gmail.com>
2018-11-12 00:25:56 +0300

    Created design and basic functionality

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <v-app id='inspire'>
+    <modal-component/>
+    <router-view/>
+    </v-app>
+  </div>
+</template>
+
+<script>
+import ModalComponent from './components/ModalComponent';
+export default {
+  name: 'App',
+  components: { ModalComponent },
+};
+</script>
+
+<style src='vuetify/dist/vuetify.min.css'>
+
+</style>
+
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/ModalComponent.vue
@@ -0,0 +1,110 @@
+
+<template>
+<div>
+    <v-layout row justify-center>
+      <v-dialog v-model="dialog"  persistent max-width="700" >
+        
+        <v-btn slot="activator" color="primary" dark>Open Dialog</v-btn>
+      
+      <v-card > 
+
+        <div class = "header-logo">
+        <v-flex xs3 >
+     
+        <v-img class="v-img-logo" 
+          src="https://argos1c.ru/templates/argos/images/logo.png"
+          
+        >
+        </v-img>
+      
+        </v-flex>
+        </div>
+
+          <v-card-text>
+            <v-container grid-list-md >
+              <v-layout wrap>
+
+                <v-flex xs12>
+                  <v-text-field label="Контактный телефон" ></v-text-field>
+                </v-flex>
+
+                <v-flex xs12>
+                  <v-select
+                  :append-icon="null"
+                   clearable
+                    :items="['Тема 1', 'Тема 2', 'Тема 3', 'Тема 4']"
+                    label="Тема обращения"
+                    required
+                    
+                  ></v-select>
+                </v-flex>
+
+                <v-flex xs12>
+                  <v-textarea 
+                  rows='3'
+                  label="Пожалуйста, опишите вашу проблему и мы свяжемся с вами" required ></v-textarea>
+                </v-flex>
+              
+              </v-layout>
+            </v-container>
+            <small></small>
+          </v-card-text>
+
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            
+            <v-btn class="v-btn-save" color="#3f66b2" @click.native="dialog = false">ОТПРАВИТЬ ЗАЯВКУ</v-btn>
+          </v-card-actions>
+
+        </v-card>
+      </v-dialog>
+    </v-layout>
+  </div>
+</template>
+
+
+<script>
+  export default {
+    data () {
+      return {
+        dialog: false
+        
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+  .v-btn-save {
+    
+    top: 50%;
+    right: 4%;
+    margin-bottom: 30px;
+    margin-top: -40px;
+    width: 625px;
+    color: white;
+    font-weight: bold;
+  }
+
+  .header-logo {
+    
+    height: 80px;
+    margin-bottom: -50px;
+    background: rgb(175,175,175);
+    background: linear-gradient(180deg, rgba(213,213,225,1) 0%, rgba(228,228,235,1) 50%, rgba(255,255,255,1) 100%);
+    
+  }
+
+  .v-img-logo {
+
+    left: 40px;
+    top: 10px;
+    width: 80%;
+    height: auto;
+  }
+
+
+
+</style>
+

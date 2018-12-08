e2cd1770237b064a1978b3cb38f5b5a73dce80a3
kareni3 <e.ripachev@gmail.com>
2018-12-09 01:47:35 +0300

    Добавли поиск нужных ссылок со статьями. Осталось их спарсить

--- server.js
+++ server.js
@@ -1,75 +1,209 @@
 const tress = require('tress');
 const request = require('request');
 const ic = require('iconv-lite');
+const JSSoup = require('jssoup').default;
 
 // Взять из кукисов
-const PHPSESSID = '115uu334kk8b4lvqsub0r2qgf0'
+const PHPSESSID = 'f84oafol085s4michvor1og454'
 const ROOT_URL = 'https://its.1c.ru';
 const P = '7a31499e';
 const U = '54389-40';
 
-// массив типа с полученными данными, еба
-let results = [];
-//Внимание русские символы пробелы и всяка чушь в формате unicode пожалуйста сюда..
-//Эта юрлка полетит на обработку парсером
-let urls = [
-    '/db/content/accnds/src/%D0%BD%D0%B4%D1%81%201%D1%81%202012%20%D0%B8%D1%81%D0%BF%D1%80%D0%B0%D0%B2%D0%BB%D0%B5%D0%BD%D0%B8%D0%B5%20%D0%BF%D0%BE%D0%BA%D1%83%D0%BF%D0%BA%D0%B8%20%D1%81%D0%BB%D0%BF%D0%B5%D1%80%D0%B8%D0%BE%D0%B4%20%D0%B2%D1%8B%D1%87%D0%B5%D1%82%D0%B0%D0%BD%D0%B5%D1%82.htm?_=1542292886',
-    '/db/content/pubvnedrset/src/copyright.html?_=1539695781',
-    '/db/content/ka22doc/src/%D0%B2%D0%B2%D0%B5%D0%B4%D0%B5%D0%BD%D0%B8%D0%B5.htm?_=1542292893',
-    '/db/ka22doc#content:310:hdoc',
-    '/db/content/marketingdoc/src/%D0%B2%D0%B2%D0%B5%D0%B4%D0%B5%D0%BD%D0%B8%D0%B5.htm?_=1539857480',
-    '/db/accnds#content:1299:hdoc',
-    '/db/marketingdoc/content/4/hdoc?bus&1542293333=&event[name]=mode',
-    '/db/accnds#content:1050:hdoc',
-    '/db/declprib/content/15388/hdoc/',
-]
+/*
+const data = {
+    'openid.auth.check':	'true',
+    'openid.auth.pwd':	P,
+    'openid.auth.short':	'false',
+    'openid.auth.user':	U,
+    'openid.claimed_id':	'http://specs.openid.net/auth/2.0/identifier_select',
+    'openid.identity':	'http://specs.openid.net/auth/2.0/identifier_select',
+    'openid.mode':	'checkid_immediate',
+    'openid.ns':	'http://specs.openid.net/auth/2.0',
+    'openid.realm':	'https://its.1c.ru/login/?action=afterlogin&provider=fresh',
+    'openid.return_to':	'https://its.1c.ru/login/?action=afterlogin&provider=fresh&backurl=%2Fsection%2Fall'
+};
+const cookies = 
+    'BITRIX_SM_LOGIN=partweb;'+
+    'PHPSESSID='+PHPSESSID+';'+
+    'SUBSCRIBE_PERIOD=31.12.2018;'+
+    'USER_TYPE=:1:2:20:21:25:203:801:;'+
+    'PARTWEB_LOGIN='+U+';';
+*/
 
-// записываем в куки PHPSESSID
-const j = request.jar();
-const cookie = request.cookie(`PHPSESSID=${PHPSESSID}`);
-j.setCookie(cookie, ROOT_URL);
+const main = async () => {
+    // записываем в куки PHPSESSID
+    const j = request.jar();
+    const cookie = request.cookie(`PHPSESSID=${PHPSESSID}`);
+    //const cookie = cookies;
+    j.setCookie(cookie, ROOT_URL);
+    
+    const mainResponse = await new Promise(resolve => {
+        request.post(
+            {
+                url: 'https://its.1c.ru/section/all', 
+                jar: j,
+                encoding: 'binary',
+            }, (err, response, body) => {
+                let title = ic.decode(Buffer(body, 'binary'), "win1251");
+                console.log('mainResponse is ok.')
+                resolve({
+                    response, 
+                    body: title,
+                })
+            })
+      });
+    
+    const soup = new JSSoup(mainResponse.body);
+    liList = soup.findAll('li').filter(obj => {
+        if (obj.attrs && obj.attrs.id)
+            return obj.attrs.id.indexOf('section_all_url') !== -1
+        else {
+            return false
+        }
+    })
+    console.log('liList.length =', liList.length)
 
-// `tress` последовательно вызывает обработчик для каждой ссылки в очереди
-let q = tress(function(url, callback){
-    // вот наш волшебный request, который умеет в куки и гзип сжатие. 
-    // Хз пока как он себя поведет если будет парсить страницу без зжатия gqip. Не проверял.
-    request.post(
-    {
-        url: url, 
-        jar: j,
-        gzip: true,
-        encoding: 'binary',
-        headers: {
-            "Content-Type": "text/html; charset=Windows-1251",
-            "Content-Encoding": "gzip",
-            'transfer-encoding': 'chunked',
-          }
-          
-    } , (err, response, body) => {
-                    //console.log(body)
-                    //console.log(response.statusCode)
-                    if (err){
-                        // Ошибки надо по нормальному обработать. Пока это так.
-                        results.push('!!!! ' + err);
-                        throw err;
-                    } 
-                    // Декодируем в утф
-                    let title = ic.decode(Buffer(body, 'binary'), "win1251");
-                    // Хуярим в результаты. строку из статус кода и результата
-                    results.push(response.statusCode + ': ' + title);
-                    // вызываем callback в конце. О да, колбек. люблю его
-                    callback();
+    // сюда будем класть ссылки на разделы
+    const level1links = []
+    // сюда будем класть названия разделов
+    const level1titles = []
+    
+    for (const li of liList) {
+        let level1link = ""
+        let a = li.find('a')
+        if (a.attrs && a.attrs.href) {
+            if  (a.attrs.href.indexOf('http') === -1) {
+                level1link = ROOT_URL + a.attrs.href
+            }
+            else {
+                if (
+                    a.attrs.href.indexOf('download') !== -1 || 
+                    a.attrs.href.indexOf('1c') === -1 && 
+                    a.attrs.href.indexOf('v8') === -1
+                    )
+                    continue
+                if (a.attrs.href === 'https://its.1c.ru/db/pubunfreal')
+                    break
+                level1link = a.attrs.href
+            }
+        }
+        level1titles.push(a.contents[0]._text)
+        level1links.push(level1link)
+    }
+    console.log('level1links.length =', level1links.length)
+    console.log('level1titles.length =', level1titles.length)
+
+    let title
+    for (const [index, url] of level1links.entries()) {
+        const level1response = await new Promise(resolve => {
+            request.post(
+            {
+                url: url, 
+                jar: j,
+                gzip: true,
+                encoding: 'binary',
+                headers: {
+                    "Content-Type": "text/html; charset=Windows-1251",
+                    "Content-Encoding": "gzip",
+                    'transfer-encoding': 'chunked',
+                  }
+            } , (err, response, body) => {
+                if (err){
+                    console.log('\x1b[31mЭй ёпта! Чел, ERROR! %s\x1b[0m', e)
+                    return
+                } 
+                //выводим в консоль инфу со статусом
+                console.log(response.statusCode === 200 ? '\x1b[32m%s\x1b[0m' : '\x1b[33m%s\x1b[0m',
+                            `Index ${index+1}/${level1links.length} fetched ${url} with ${response.statusCode}`)
+                title = ic.decode(Buffer(body, 'binary'), "win1251");
+                resolve({
+                    response, 
+                    body: title,
+                })
+            });
+        });
+        const soup2 = new JSSoup(level1response.body);
+        // select нету в jssoup
+        const listLevel2 = soup2.findAll('a').filter(obj => {
+            if (obj.attrs && obj.attrs.class) {
+                return obj.attrs.class.search(/[(icon1)(icon2)(icon3)(icon4)(icon5)]/i) !== -1
+            }
+            else {
+                return false
+            }
+        })
+        const pagesCounter = listLevel2.length
+        let fetchedPagesCounter = 0
+        console.log('listLevel2.length =', pagesCounter)
+        const level2links = []
+        const level2titles = []
+
+        for (const a of listLevel2) {
+            if (a.attrs && a.attrs.href) {
+                if (a.contents[0]) {
+                    level2titles.push(a.contents[0]._text)
+                    // так надо?
+                } else {
+                    level2titles.push(undefined)
+                }
+                if  (a.attrs.href.indexOf('http') === -1) {
+                    level2links.push(ROOT_URL + a.attrs.href);
+                }
+                else {
+                    if (a.attrs.href.indexOf('download') !== -1) continue
+                    level2links.push(a.attrs.href);
+                }
+            }
+        }
+    }
+
+
+
+
+    // `tress` последовательно вызывает обработчик для каждой ссылки в очереди
+    let q = tress(function(url, callback){
+        // вот наш волшебный request, который умеет в куки и гзип сжатие. 
+        // Хз пока как он себя поведет если будет парсить страницу без зжатия gqip. Не проверял.
+        request.post(
+        {
+            url: url, 
+            jar: j,
+            gzip: true,
+            encoding: 'binary',
+            headers: {
+                "Content-Type": "text/html; charset=Windows-1251",
+                "Content-Encoding": "gzip",
+                'transfer-encoding': 'chunked',
+              }
+        } , (err, response, body) => {
+            if (err){
+                console.log('\x1b[31mЭй ёпта! Чел, ERROR! %s\x1b[0m', e)
+                return
+            } 
+            
+            
+        /* 
+            // Декодируем в утф
+            let title = ic.decode(Buffer(body, 'binary'), "win1251");
+            // Хуярим в результаты. строку из статус кода и результата
+            results.push(response.statusCode + ': ' + title);
+            // вызываем callback в конце. О да, колбек. люблю его
+            */
+            callback();
+        });
     });
-});
-
-// эта функция выполнится, когда в очереди закончатся ссылки
-// есть идея делать несколько очередей, чтоб не переполнять стек. И записывать кусками. пока в теории все.
-// записывает в json, никто не мешает сделать что хочется
-q.drain = function(){
-    require('fs').writeFileSync('./data.json', JSON.stringify(results, null, 4));
+    
+    q.drain = function(){
+        //require('fs').writeFileSync('./data.json', JSON.stringify(results, null, 4));
+    }
+    
+    /* level1links.forEach( (url) => {
+        q.push(url);
+    }); */
 }
 
-// а вот и очередь. Можно например таймаут юзать. 
-urls.forEach( (url) => {
-    q.push(ROOT_URL+url);
-});
+
+
+
+
+main();

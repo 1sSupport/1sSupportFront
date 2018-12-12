62f861bb56706b1d43852d1905213731d6db0803
fi1tch <vilkovaliza@gmail.com>
2018-12-13 00:21:29 +0300

    home page structure added

--- src/components/SearchResult.vue
+++ src/components/SearchResult.vue
@@ -5,22 +5,10 @@
   
           <v-list two-line>
             <template v-for="(item, index) in items">
-              <v-subheader
-                v-if="item.header"
-                :key="item.header"
-              >
-                {{ item.header }}
-              </v-subheader>
-  
-              <v-divider
-                v-else-if="item.divider"
-                :inset="item.inset"
-                :key="index"
-              ></v-divider>
-  
+
                 <v-list-tile-content>
-                  <v-list-tile-title v-html="item.title"></v-list-tile-title>
-                  <v-list-tile-sub-title v-html="item.subtitle"></v-list-tile-sub-title>
+                  <v-list-tile-title class='text' v-html="item.title"></v-list-tile-title>
+                  <v-list-tile-sub-title class='subtitle' v-html="item.subtitle"></v-list-tile-sub-title>
                 </v-list-tile-content>
               </v-list-tile>
             </template>
@@ -39,13 +27,13 @@ name: "SearchResult",
       items: [
         {
           
-          title: 'Название статьи',
-          subtitle: "<Очень важное описание статьи"
+          title: "<class='text'>Название статьи",
+          subtitle: "Посадил дед репку. Выросла репка большая-пребольшая. Стал дед репку из земли тащить: тянет-потянет, вытянуть не может. Позвал дед бабку. Бабка за дедку, дедка за репку — тянут-потянут, вытянуть не могут. Позвала бабка внучку. Внучка за бабку, бабка за дедку, дедка за репку — тянут-потянут, вытянуть не могут."
         },
-        { divider: true, inset: true },
+        { divider: true, inset: false },
         {
-          title: 'Название статьи2',
-          subtitle: "Очень важное описание статьи2"
+          title: "<span class='text-title'>Название статьи",
+          subtitle: "Кликнула внучка Жучку. Жучка за внучку, внучка за бабку, бабка за дедку, дедка за репку — тянут-потянут, вытянуть не могут. Кликнула Жучка кошку. Кошка за Жучку, Жучка за внучку, внучка за бабку, бабка за дедку, дедка за репку — тянут-потянут, вытянуть не могут. Позвала кошка мышку. Мышка за кошку, кошка за Жучку, Жучка за внучку, внучка за бабку, бабка за дедку, дедка за репку — тянут-потянут, вытащили репку!"
         },
         { divider: true, inset: true },
 
@@ -56,3 +44,17 @@ name: "SearchResult",
 
 
 </script>
+
+<style scoped>
+    .text{
+        margin-left: 3%;
+        font-size: 20px;
+        font-weight: bold;
+    }
+
+    .subtitle {
+        margin-left: 3%;
+        margin-top: 1%;
+        width: 90%;
+    }
+</style>

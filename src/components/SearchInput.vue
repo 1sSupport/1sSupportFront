3a6cf5e391591087009e6afc8932bcdfe6ba0a89
AnnRatn <annratnikova@mail.ru>
2018-12-13 21:49:05 +0300

    Hints adding

--- src/components/SearchInput.vue
+++ src/components/SearchInput.vue
@@ -10,10 +10,33 @@
           <v-card-actions>
             <v-layout>
               <v-flex xs8>
-                <v-text-field v-model="searchString" label="Search" solo clearable></v-text-field>
+                <v-text-field
+                  v-model="searchString"
+                  label="Search"
+                  solo
+                  clearable
+                  hide-details
+                  @input="HintSelections(searchString)"
+                ></v-text-field>
+                <v-list>
+                  <v-list-tile
+                    v-for="item in shownHint"
+                    :key="item.id"
+                    @click="PickHint(item.text)"
+                    v-if="showHints"
+                  >
+                    <v-list-tile-content>
+                      <v-list-tile-title v-html="item.text"></v-list-tile-title>
+                    </v-list-tile-content>
+                  </v-list-tile>
+                </v-list>
               </v-flex>
               <v-flex xs2>
-                <v-btn color="#3f66b2" style="height: 47px">
+                <v-btn
+                  color="#3f66b2"
+                  style="height: 47px"
+                  @click.native.stop="GetArticles(searchString)"
+                >
                   <v-icon dark>search</v-icon>
                 </v-btn>
               </v-flex>
@@ -29,29 +52,80 @@
 export default {
   data() {
     return {
-      searchString: ""
+      searchString: "",
+      usersView: true,
+      showHints: false,
+      shownHint: [],
+      overlapOldHint: [],
+      overlapNewHint: [],
+      allHint: [
+        {
+          id: 1,
+          text: "иностранцы"
+        },
+        {
+          id: 2,
+          text: "налоги"
+        },
+        {
+          id: 3,
+          text: "иностранцы1"
+        },
+        {
+          id: 4,
+          text: "иииии"
+        },
+        {
+          id: 5,
+          text: "иноаааа"
+        },
+        {
+          id: 6,
+          text: "налоги78888"
+        }
+      ]
     };
+  },
+  mounted() {
+    this.overlapNewHint = this.allHint;
+  },
+  methods: {
+    PickHint(text) {
+      this.searchString = text;
+      this.showHints = false;
+    },
+    HintSelections(searchString) {
+      //alert(searchString);
+      this.overlapOldHint = this.overlapNewHint;
+      this.overlapNewHint = [];
+      for (var i = 0; i < this.overlapOldHint.length; i++) {
+        if (this.overlapOldHint[i].text.startsWith(searchString)) {
+          this.overlapNewHint.push(this.overlapOldHint[i]);
+          console.log(this.overlapOldHint[i]);
+        }
+      }
+      if (this.overlapNewHint.length >= 5) {
+        this.shownHint = this.overlapNewHint.slice(0, 5);
+      } else {
+        this.shownHint = this.overlapNewHint.slice(
+          0,
+          this.overlapNewHint.length
+        );
+      }
+      if (searchString != null && searchString.length != 0) {
+        this.showHints = true;
+      } else {
+        this.showHints = false;
+        this.overlapNewHint = this.allHint;
+      }
+    },
+    GetArticles(searchString) {
+      alert('Поисковой запрос по запросу "' + this.searchString + '"');
+    }
   }
 };
 </script>
 
 
-<style scoped>
-/* .v-model {
-  width: 60%;
-  margin-left: 3%;
-  margin-top: 1%;
-} */
-
-/* .SearchHeadline {
-  width: 60%;
-  margin-top: 2%;
-  margin-left: -8%;
-} */
-/* .SearchBtn {
-  margin-top: -6%;
-  margin-left: 40%;
-  color: white;
-  height: 47px;
-} */
+<style>
 </style>

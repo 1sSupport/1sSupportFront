4714f02f40c87159e26798343bf6ba01e5a70603
Gteliaeva <gteliaeva@gmail.com>
2018-12-13 16:04:10 +0300

    Статья об 1с

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+
+<template>
+  <div id="app">
+    <v-app id='inspire'>
+    <modal-component/>
+    <router-view/>
+    </v-app>
+  </div>
+</template>
+
+<script>
+import ModalComponent from './components/ModalComponent';
+export default {
+  name: 'App',
+  components: { ModalComponent },
+};
+</script>
+
+<style src='vuetify/dist/vuetify.min.css'>
+</style>
+
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+</style>

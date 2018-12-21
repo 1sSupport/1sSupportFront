03d7ad8b49b2e6bcccb636b518b93c473f454dff
Gteliaeva <38006923+Gteliaeva@users.noreply.github.com>
2018-12-21 21:20:09 +0300

    Добавила тему

--- src/main.js
+++ src/main.js
@@ -6,7 +6,17 @@ import Vuetify from 'vuetify'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
 import 'vuetify/dist/vuetify.min.css'
 
-Vue.use(Vuetify)
+Vue.use(Vuetify, {
+  theme: {
+    primary: '#003399',
+	  secondary: '#424242',
+	  accent: '#003399',
+	  error: '#FF5252',
+	  info: '#004EEB',
+	  success: '#4CAF50',
+	  warning: '#FFC107'
+  }
+})
 
 Vue.config.productionTip = false
 

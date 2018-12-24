7f3068bccca94ab5e5d596dc3aec37e1377fa4de
AnnRatn <annratnikova@mail.ru>
2018-12-24 03:58:03 +0300

    hints view was fixed

--- src/components/SearchInput.vue
+++ src/components/SearchInput.vue
@@ -10,47 +10,26 @@
           <v-card-actions>
             <v-layout row wrap>
               <v-flex xs12 sm8>
-                <!-- <v-autocomplete
-                  :items="marksItems"
-                  :menu-props="{ overflowY: false}"
-                  browser-autocomplete="on"
-                  :search-input.sync="searchString"
-                  clearable
-                  solo
-                  hide-no-data
-                  hide-details
-                  label="Поиск"
-
-                ></v-autocomplete> -->
                 <v-combobox
                   v-model="searchString"
-                  :filter="filter"
-                  :items="marksItems"
+                  @change="searchRequest($event)"
+                  no-filter
+                  :items="shownHint"
                   :search-input.sync="search"
                   label="Поиск"
                   hide-no-data
                   hide-details
                   solo
                   append-icon="null"
-
                   :menu-props="{ overflowY: false}"
                 >
-                  <template
-                    slot="item"
-                    slot-scope="{ index, item, parent }"
-                  >
-                    <v-list-tile-content>
-                        {{ item.text }}
-                    </v-list-tile-content>
+                  <template slot="item" slot-scope="{ index, item, parent }">
+                    <v-list-tile-content>{{ item }}</v-list-tile-content>
                   </template>
                 </v-combobox>
               </v-flex>
               <v-flex xs12 sm2>
-                <v-btn
-                  color="#3f66b2"
-                  style="height: 47px"
-                  @click.native.stop="GetArticles(searchString)"
-                >
+                <v-btn color="#3f66b2" style="height: 47px">
                   <v-icon medium color="white">search</v-icon>
                 </v-btn>
               </v-flex>
@@ -79,18 +58,11 @@ export default {
   data() {
     return {
       searchString: "",
-      shownHint: [],
-      // showHints: false
       search: "",
+      shownHint: []
     };
   },
-  computed: {
-    marksItems: function() {
-      return this.marks.map( (item, index) => {
-        return {"text": item, "value": item}
-      })
-    }
-  },
+  computed: {},
   mounted() {
     this.shownHint = this.marks.slice(0, 5);
   },
@@ -98,50 +70,37 @@ export default {
     searchRequest: function(payload) {
       this.$emit("search", payload);
     },
-    PickHint(text) {
-      this.searchString = text;
-      this.showHints = false;
-    },
-    HintSelections(searchString) {
-      //alert("a");
-      this.shownHint = [];
-      var search = searchString.toLowerCase();
+    newHintMas(query) {
+      var mas = [];
+      var search1 = this.searchString.toLowerCase();
       for (var i = 0; i < this.marks.length; i++) {
         var mark = this.marks[i].toLowerCase();
-        if (mark.indexOf(search) > -1 && this.shownHint.length < 5) {
-          this.shownHint.push(this.marks[i]);
+        if (mark.indexOf(search1) > -1 && mas.length < 5) {
+          mas.push(this.marks[i]);
         }
       }
-      this.showHints = true;
-    },
-    customFilter(item, queryText, itemText) {
-      return true;
-    },
-    GetArticles(searchString) {
-      // alert('Поисковой запрос по запросу "' + this.searchString + '"');
-    },
-    // this is default filter function
-    filter (item, queryText, itemText) {
-      if (item.header) return false
-
-      const hasValue = val => val != null ? val : ''
-
-      const text = hasValue(itemText)
-      const query = hasValue(queryText)
-
-      return text.toString()
-        .toLowerCase()
-        .indexOf(query.toString().toLowerCase()) > -1
+      return mas;
     }
   },
   watch: {
-    searchString (val, prev) {
-      console.log(val)
-      if ("text" in val) {
-        this.$emit("search", val.text)
-      }
-      else {
-        this.$emit("search", val)
+    searchString(val, prev) {
+      console.log(val);
+      // if ("text" in val) {
+      //   this.$emit("search", val.text);
+      // } else {
+      this.$emit("search", val);
+      //}
+    },
+    search(val, prev) {
+      console.log(val);
+
+      this.shownHint = [];
+      var search1 = val.toLowerCase();
+      for (var i = 0; i < this.marks.length; i++) {
+        var mark = this.marks[i].toLowerCase();
+        if (mark.indexOf(search1) > -1 && this.shownHint.length < 5) {
+          this.shownHint.push(this.marks[i]);
+        }
       }
     }
   }
